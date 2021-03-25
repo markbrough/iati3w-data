36c891ec5bc23657a95c0c3db5fc16bdbd08c476
David Megginson <david.megginson@gmail.com>
2021-03-25 09:34:18 -0400

    Strip whitespace around an org name.

--- index-orgs.py
+++ index-orgs.py
@@ -1,5 +1,9 @@
 import json, sys
 
+ROLES = ["implementing", "programming", "funding"]
+SECTOR_TYPES = ["dac", "humanitarian"]
+LOCATION_TYPES = ["admin1", "admin2", "unclassified"]
+
 if len(sys.argv) != 2:
     print("Usage: {} <activity-file>".format(sys.argv[0]), file=sys.stderr)
     sys.exit(2)
@@ -9,16 +13,51 @@ index = {}
 with open(sys.argv[1], "r") as input:
     activities = json.load(input)
     for activity in activities:
-        for role in ["implementing", "programming", "funding"]:
+
+        for role in ROLES:
             for org in activity["orgs"][role]:
-                index.setdefault(org, {})
-                index[org].setdefault(role, [])
-                index[org][role].append({
+
+                # Skip blank orgs
+                if not org:
+                    continue
+
+                org = org.strip();
+
+                # Add a default record if missing
+                index.setdefault(org, {
+                    "activities": {},
+                    "partners": {},
+                    "sectors": {},
+                    "locations": {},
+                })
+
+                # Index activities
+                index[org]["activities"].setdefault(role, [])
+                index[org]["activities"][role].append({
                     "identifier": activity["identifier"],
                     "title": activity["title"],
-                    "sectors": activity["sectors"],
-                    "locations": activity["locations"],
                 })
+
+                # Index partners
+                for role in ROLES:
+                    for partner in activity["orgs"][role]:
+                        if partner and (partner != org):
+                            index[org]["partners"].setdefault(partner, 0)
+                            index[org]["partners"][partner] += 1
+
+                # Index sectors
+                for type in SECTOR_TYPES:
+                    for sector in activity["sectors"][type]:
+                        if sector:
+                            index[org]["sectors"].setdefault(sector, 0)
+                            index[org]["sectors"][sector] += 1
+
+                # Index locations
+                for type in LOCATION_TYPES:
+                    for location in activity["locations"][type]:
+                        if sector:
+                            index[org]["locations"].setdefault(location, 0)
+                            index[org]["locations"][location] += 1
 
 print(json.dumps(index, indent=4))
 
