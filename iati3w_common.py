502a3f24e52fcf002e113056b23d5fe8e96fe65e
David Megginson <david.megginson@gmail.com>
2021-03-29 11:07:29 -0400

    Add functions to normalise strings and tokens.

--- iati3w_common.py
+++ iati3w_common.py
@@ -2,7 +2,25 @@
 
 """
 
+import re
+
 ROLES = ["implementing", "programming", "funding"]
 SECTOR_TYPES = ["dac", "humanitarian"]
 LOCATION_TYPES = ["admin1", "admin2", "unclassified"]
+
+
+def normalise_string (s):
+    """ Normalise whitespace in a string.
+    Preserve character case and punctuation
+
+    """
+    return re.sub(r'\W+', ' ', s.strip())
+
+def tokenise_string (s):
+    """ Create a lookup token from a string.
+    Normalise space, convert to lowercase, and remove punctuation
+
+    """
+    return re.sub(r'[^\w+]', ' ', s.strip().lower())
+               
 
