7377c5ae331446e5952fe76379bd27b55f16bceb
David Megginson <david.megginson@gmail.com>
2021-04-30 17:54:22 -0400

    Simplify network generation.

--- gen-network.py
+++ gen-network.py
@@ -1,5 +1,7 @@
 import json, sys
 
+from iati3w_common import make_token
+
 ACTIVITIES = "output/activities.json"
 ORG_INDEX = "output/org-index.json"
 
@@ -16,25 +18,35 @@ with open(ACTIVITIES, "r") as input:
 with open(ORG_INDEX, "r") as input:
     org_index = json.load(input)
 
+def get_org_stubs (activity):
+    result = set()
+    for role in activity["orgs"]:
+        for stub in activity["orgs"][role]:
+            result.add(stub)
+    return result
+    
+def make_node (org):
+    return (org["info"]["shortname"][:32], GROUPS[org["info"]["scope"]],)
+
+def make_link_key (org1, org2):
+    return (org1["info"]["shortname"][:32], org2["info"]["shortname"][:32],)
+
 
 nodes = set()
+
 links = {}
     
 for activity in activities.values():
 
-    # Get all the orgs in the activity
-    org_names = set()
-    for type, names in activity["orgs"].items():
-        for name in names:
-            org_names.add(name)
-            org = org_index[name]
-            nodes.add((name, GROUPS[org["info"]["scope"]],))
-
-    org_names = sorted(list(org_names))
+    org_names = sorted(list(get_org_stubs(activity)))
     for i in range(0, len(org_names)):
+        org1 = org_index[make_token(org_names[i])]
         for j in range(i + 1, len(org_names)):
-            key = (org_names[i], org_names[j],)
+            org2 = org_index[make_token(org_names[j])]
+            key = make_link_key(org1, org2)
             links[key] = links.get(key, 0) + 1
+            nodes.add(make_node(org1))
+            nodes.add(make_node(org2))
 
 result = {
     "nodes": [],
