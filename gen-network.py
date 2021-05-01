de6c315ad5275999837b295a4d465212cae29097
David Megginson <david.megginson@gmail.com>
2021-05-01 15:21:04 -0400

    Update to include more info about orgs.

--- gen-network.py
+++ gen-network.py
@@ -6,24 +6,10 @@ from iati3w_common import make_token
 
 ORG_INDEX = "output/org-index.json"
 
-GROUPS = {
-    "local": 3,
-    "regional": 2,
-    "international": 1,
-    "unknown": 4,
-}
-
 with open(ORG_INDEX, "r") as input:
     org_index = json.load(input)
 
-def make_node (org):
-    return (org["info"]["shortname"][:32], GROUPS[org["info"]["scope"]],)
-
-def make_link_key (org1, org2):
-    return (org1["info"]["shortname"][:32], org2["info"]["shortname"][:32],)
-
-
-nodes = set()
+orgs_seen = set()
 
 links = {}
     
@@ -38,10 +24,10 @@ for org in org_index.values():
                 partner = org_index[stub]
                 if partner is None or partner["info"].get("skip", False):
                     continue
-                key = make_link_key(org, partner)
+                key = (org["info"]["stub"], partner["info"]["stub"],)
                 links[key] = links.get(key, 0) + 1
-                nodes.add(make_node(org))
-                nodes.add(make_node(partner))
+                orgs_seen.add(org["info"]["stub"])
+                orgs_seen.add(partner["info"]["stub"])
 
 
 result = {
@@ -49,8 +35,14 @@ result = {
     "links": [],
 }
 
-for entry in nodes:
-    result["nodes"].append({"id": entry[0], "group": entry[1]})
+for stub in orgs_seen:
+    org = org_index[stub]
+    result["nodes"].append({
+        "stub":stub,
+        "name": org["info"]["name"],
+        "shortname": org["info"]["shortname"],
+        "scope": org["info"]["scope"],
+    })
 
 for entry, value in links.items():
     result["links"].append({"source": entry[0], "target": entry[1], "value": value})
